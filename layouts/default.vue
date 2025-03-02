cbea84f3f0e3a577cc13b426da77355b5969b77f
adams eugene <adamseugene292@gmail.com>
2025-03-02 21:59:11 +0000

    updated the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="w-full">
+    <div class="w-full h-screen flex flex-col overflow-hidden">
         <SharedToastContainer />
-        <div class="sticky top-0 z-50">
+        <div class="fixed top-0 z-50 w-full">
             <LandingHeader />
         </div>
-        <div class="w-full overflow-x-hidden overflow-y-auto">
+        <div class="w-full flex-1 overflow-x-hidden overflow-y-auto mt-[90px]">
             <slot name="default" />
         </div>
     </div>
